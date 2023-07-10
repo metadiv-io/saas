260d10ccde875748d77b8949d764e98002697bf1
Nelson Lai <nelsonlai9503@gmail.com>
2023-07-10 12:18:21 +0800

    refactor checking env var

--- constant/check.go
+++ constant/check.go
@@ -7,16 +7,15 @@ import (
 )
 
 // CheckEnvSet checks if the environment variable is set.
-// If not, it panics. (Skip if GIN_MODE == test)
+// If not, it panics. (Only in debug or release mode)
 func CheckEnvSet(e interface{}, hint ...string) {
-	if env.String("GIN_MODE") == "test" {
-		return
-	}
-	if reflect.ValueOf(e).IsZero() {
-		if len(hint) > 0 {
-			panic(hint[0])
-		} else {
-			panic("an environment variable is required")
+	if env.String("GIN_MODE") == "release" || env.String("GIN_MODE") == "debug" {
+		if reflect.ValueOf(e).IsZero() {
+			if len(hint) > 0 {
+				panic(hint[0])
+			} else {
+				panic("an environment variable is required")
+			}
 		}
 	}
 }
