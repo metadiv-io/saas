658016e491db401131f42d22f789bd8cf4b0bdf3
Nelson Lai <nelsonlai9503@gmail.com>
2023-09-24 22:23:18 +0800

    refactor ping api

--- service/ping.go
+++ service/ping.go
@@ -1,17 +1,26 @@
 package service
 
-import "github.com/metadiv-io/saas/micro"
+import (
+	"github.com/metadiv-io/saas/micro"
+	"github.com/metadiv-io/saas/types"
+)
 
 type Pong struct {
-	SystemUUID string `json:"system_uuid"`
-	SystemName string `json:"system_name"`
+	SystemUUID string      `json:"system_uuid"`
+	SystemName string      `json:"system_name"`
+	Apis       []types.Api `json:"apis"`
 }
 
 func PingHandler() micro.Service[struct{}] {
 	return func(ctx *micro.Context[struct{}]) {
+		apis := make([]types.Api, 0)
+		for _, api := range micro.UsageManager.UUIDToApi {
+			apis = append(apis, *api)
+		}
 		ctx.OK(&Pong{
 			SystemUUID: ctx.Engine.GingerEngine.SystemUUID,
 			SystemName: ctx.Engine.GingerEngine.SystemName,
+			Apis:       apis,
 		})
 	}
 }
