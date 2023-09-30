5ff1375efbfb5df20393b7c492b8b3242e37e519
Nelson Lai <nelsonlai9503@gmail.com>
2023-09-30 14:12:23 +0800

    json ignore gorm.DeletedAt

--- micro/model.go
+++ micro/model.go
@@ -87,7 +87,7 @@ type Model struct {
 	ID        uint           `gorm:"primarykey" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 type BaseWorkspaceModel struct {
